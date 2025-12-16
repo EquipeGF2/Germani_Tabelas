268812ffdd37e0f5f478a074028b9830e699afe5
genaroforratig365-pixel <genaroforratig365@gmail.com>
2025-12-16 17:09:42 -0300

    Create 20251216193000_v2_regras_logistica_pauta.down.sql

---
+++ migrations/20251216193000_v2_regras_logistica_pauta.down.sql
@@ -0,0 +1,11 @@
+PRAGMA foreign_keys = OFF;
+
+DROP TABLE IF EXISTS preco_movimentos;
+DROP TABLE IF EXISTS pauta_itens;
+DROP TABLE IF EXISTS regras_subtabela;
+DROP TABLE IF EXISTS custos_logisticos;
+DROP TABLE IF EXISTS st_regras;
+DROP TABLE IF EXISTS ncm_categorias;
+DROP TABLE IF EXISTS destinos;
+
+PRAGMA foreign_keys = ON;
